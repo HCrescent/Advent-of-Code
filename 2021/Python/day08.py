4e0d19ece43b237c3d894402c34289b727c6b968
HCrescent <HCrescentMoon@gmail.com>
2021-12-09 21:49:47 -0600

    Day 8 solution finished

--- 2021/Python/day08.py
+++ 2021/Python/day08.py
@@ -20,40 +20,58 @@ def part2(output_data):
 	for each in data:
 		data_p2.append(each[:58])
 	data_p2 = [data_p2[_].split(' ') for _ in range(len(data_p2))]
-	print(data_p2)
 	output_sum = 0
 	for index, words in enumerate(data_p2):
 		output_sum += solve(words, output_data[index])
 	return output_sum
 
 
-def solve(in_keys, out_keys):
-	display_dict = {}
-	for each in in_keys:
+def solve(in_words, out_words):
+	setlist = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+	# first pass will always get 4 unique digits
+	for each in in_words:
 		if len(each) == 2:
-			display_dict[each] = 1
+			setlist[1] = each
+			one = set(each)
 		elif len(each) == 3:
-			display_dict[each] = 7
+			setlist[7] = each
 		elif len(each) == 4:
-			display_dict[each] = 4
+			setlist[4] = each
+			four = set(each)
 		elif len(each) == 7:
-			display_dict[each] = 8
-	solve_three(in_keys, display_dict)
-	print(display_dict)
-	return 19
-
-
-def solve_three(in_keys, display_dict):
-	for each in in_keys:
-		if len(each) == 5:
-			for letter in (key for key, num in display_dict.copy().items() if num == 1):
-				if letter[0] and letter[1] in each:
-					display_dict[each] = 3
-	pass
+			setlist[8] = each
+	# second pass gets all digits except 5 and 2
+	for each in in_words:
+		if each not in setlist:  # skip known keys
+			if len(each) == 5:
+				if one < set(each):
+					setlist[3] = each
+			elif len(each) == 6:
+				if four < set(each):
+					setlist[9] = each
+				elif one < set(each):
+					setlist[0] = each
+				else:
+					setlist[6] = each
+					six = set(each)
+					final_piece = one - six  # final_piece is the set element to determine 5 and 2
+	# third pass tests and assigns 5 and 2 against our final_piece
+	for each in in_words:
+		if each not in setlist:  # skip known keys
+			if len(each) == 5:
+				if final_piece < set(each):
+					setlist[2] = each
+				else:
+					setlist[5] = each
+	decoded_number = ""
+	for code in out_words:
+		for i, word in enumerate(setlist):
+			if set(code) == set(word):
+				decoded_number += str(i)
+	return int(decoded_number)
 
 
 if __name__ == "__main__":
 	part1, readings = part1()
-	part2(readings)
 	print("part 1: ", part1)
-# print("part 2: ")
+	print("part 2: ", part2(readings))
