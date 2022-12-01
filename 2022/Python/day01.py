f93c2e5186c16759e1aeecc4ac833acfc55cae5c
HCrescent <HCrescentMoon@gmail.com>
2022-11-30 23:54:25 -0600

    2022 day 1 solution cleaned up a bit

--- 2022/Python/day01.py
+++ 2022/Python/day01.py
@@ -1,23 +1,16 @@
 """Day 01 Advent_of_Code 2022"""
 with open("input/day01.txt", 'r') as infile:
-	data = [line.rstrip() for line in infile]
-
-
-def fun(data):
-	sums = []
-	accumulator = 0
-	for each in data:
-		if each != '':
-			accumulator += int(each)
-		else:
-			sums.append(accumulator)
-			accumulator = 0
-	return sums
-
+	data = [int(line.rstrip()) if line != '\n' else 0 for line in infile]
 
 if __name__ == "__main__":
-	elf_list = fun(data)
-	tmp = max(elf_list)
-	print("part 1: ", tmp)
+	elf_list = []
+	accumulator = 0
+	for each in data:
+		if each:
+			accumulator += each
+		else:
+			elf_list.append(accumulator)
+			accumulator = 0
+	print("part 1: ", max(elf_list))
 	elf_list.sort()
 	print("part 2: ", sum(elf_list[-3:]))
