b46a337b7d6c227815b97c7ff3897762ee2665bd
HCrescent <HCrescentMoon@gmail.com>
2022-12-04 00:38:02 -0600

    2022 day 4 complete tweaked some expressions around

--- 2022/Python/day04.py
+++ 2022/Python/day04.py
@@ -1,15 +1,14 @@
 """Day 04 Advent_of_Code 2022"""
 with open("input/day04.txt", 'r') as infile:
-	data = [line.rstrip().split(',') for line in infile]
+	data = [list(map(int, line.rstrip().replace(',', '-').split('-'))) for line in infile]
 
 
 def part1():
 	count = 0
 	for line in data:
-		start, end = map(int, line[0].split('-'))
+		start, end, start2, end2 = line
 		elf1_set = {_ for _ in range(start, end+1)}
-		start, end = map(int, line[1].split('-'))
-		elf2_set = {_ for _ in range(start, end+1)}
+		elf2_set = {_ for _ in range(start2, end2+1)}
 		if elf1_set.issubset(elf2_set) or elf2_set.issubset(elf1_set):
 			count += 1
 	return count
@@ -18,10 +17,9 @@ def part1():
 def part2():
 	count = 0
 	for line in data:
-		start, end = map(int, line[0].split('-'))
+		start, end, start2, end2 = line
 		elf1_set = {_ for _ in range(start, end+1)}
-		start, end = map(int, line[1].split('-'))
-		elf2_set = {_ for _ in range(start, end+1)}
+		elf2_set = {_ for _ in range(start2, end2+1)}
 		if elf1_set.intersection(elf2_set) != set():
 			count += 1
 	return count
