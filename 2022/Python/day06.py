fc56803fe772b13c13027257a9e35faab3c74901
HCrescent <HCrescentMoon@gmail.com>
2022-12-05 23:22:52 -0600

    2022 day 6 complete condensed

--- 2022/Python/day06.py
+++ 2022/Python/day06.py
@@ -3,20 +3,13 @@ with open("input/day06.txt", 'r') as infile:
 	data = [line.rstrip() for line in infile]
 
 
-def part1():
+def firstUniques(capture):
 	for i in range(len(data[0])):
-		detector_set = {each for each in data[0][i:i+4]}
-		if len(detector_set) == 4:
-			return i+4
-
-
-def part2():
-	for i in range(len(data[0])):
-		detector_set = {each for each in data[0][i:i+14]}
-		if len(detector_set) == 14:
-			return i+14
+		detector_set = {each for each in data[0][i:i+capture]}
+		if len(detector_set) == capture:
+			return i+capture
 
 
 if __name__ == "__main__":
-	print("part 1: ", part1())
-	print("part 2: ", part2())
+	print("part 1: ", firstUniques(4))
+	print("part 2: ", firstUniques(14))
