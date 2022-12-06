37af7f25f2dacf6e1402a28b18a3792a56949c12
HCrescent <HCrescentMoon@gmail.com>
2022-12-06 17:33:48 -0600

    2021 day 16 part 2 touched up complete

--- 2021/Python/day16.py
+++ 2021/Python/day16.py
@@ -24,7 +24,12 @@ def hex_to_bin(packet):
 	return "".join([hex_conversion[char] for char in packet])
 
 
-def parsePacketBinPart1(binary_string):
+def parsePacketBin(binary_string):
+	""" recursive packet interpreter
+
+	:param binary_string: Str - binary data to be processed based on packet structure
+	:return: Tuple - (Int, Int, Int) - (evaluated expression result, version sum, packet index)
+	"""
 	slicer = 6
 	ver_sum = int(binary_string[:3], base=2)  # add the version number to the running version sum
 	packet_type = binary_string[3:6]
@@ -37,25 +42,25 @@ def parsePacketBinPart1(binary_string):
 			else:  # we hit the first 0 grab the last bits
 				build_literal += binary_string[slicer+1:slicer+5]
 				pkt_len = slicer+5
-				return int(build_literal, base=2), ver_sum, pkt_len  # return literal value, and the running version sum
+				return int(build_literal, base=2), ver_sum, pkt_len  # return literal value, running sum, packet length
 		case _:  # operator packet
-			literal_list = []  # ultimately we will build a string of the required operations and literals
+			literal_list = []  # for storing our literals for our operators
 			match binary_string[slicer]:  # check the Length Type ID
 				case "0":  # next 15 bits indicate length of bits of sub-packets
 					subPacket_bit_endpoint = int(binary_string[slicer+1:slicer+16], base=2) + slicer + 16
 					packet_i = slicer+16
-					while packet_i < subPacket_bit_endpoint:
-						tmp_literal, tmp_vSum, pkt_delta = parsePacketBinPart1(binary_string[packet_i:])
-						literal_list.append(tmp_literal)  # build the instruction string
-						ver_sum += tmp_vSum  # update teh version sum for part 1
+					while packet_i < subPacket_bit_endpoint:  # until we reach end of sub-packets
+						tmp_literal, tmp_vSum, pkt_delta = parsePacketBin(binary_string[packet_i:])
+						literal_list.append(tmp_literal)  # add literal to list for processing
+						ver_sum += tmp_vSum  # update the version sum for part 1
 						packet_i += pkt_delta  # the next pkt index is updated by the length of the previous pkt
 				case _:  # next 11 bits indicate total number of sub-packets
 					total_subPackets = int(binary_string[slicer+1:slicer+12], base=2)
 					packet_i = slicer+12  # indexer for start of packet
 					for _ in range(total_subPackets):  # for each sub-packet
-						tmp_literal, tmp_vSum, pkt_delta = parsePacketBinPart1(binary_string[packet_i:])
-						literal_list.append(tmp_literal)  # build the instruction string
-						ver_sum += tmp_vSum  # update teh version sum for part 1
+						tmp_literal, tmp_vSum, pkt_delta = parsePacketBin(binary_string[packet_i:])
+						literal_list.append(tmp_literal)  # add literal to list for processing
+						ver_sum += tmp_vSum  # update the version sum for part 1
 						packet_i += pkt_delta  # the next pkt index is updated by the length of the previous pkt
 			match packet_type:
 				case "000":  # sum packet
@@ -78,5 +83,5 @@ def parsePacketBinPart1(binary_string):
 
 
 if __name__ == "__main__":
-	print("part 1: ", parsePacketBinPart1(hex_to_bin(data))[1])
-	print("part 2: ", parsePacketBinPart1(hex_to_bin(data))[0])
+	print("part 1: ", parsePacketBin(hex_to_bin(data))[1])
+	print("part 2: ", parsePacketBin(hex_to_bin(data))[0])
