b0f6c055d0925501947eaa6a03183d99d7eebf8b
HCrescent <HCrescentMoon@gmail.com>
2022-12-08 00:13:42 -0600

    2022 day 8 part 2 done

--- 2022/Python/day08.py
+++ 2022/Python/day08.py
@@ -1,49 +1,41 @@
 """Day 08 Advent_of_Code 2022"""
 with open("input/day08.txt", 'r') as infile:
-	data = [line.rstrip() for line in infile]
-matrix = [[int(each) for each in line] for line in data]
+	data = [[int(each) for each in line.rstrip()] for line in infile]
 
 
-def fun(grid):
-	for each in grid:
-		print(each)
-
-
-def treeVisible(grid, height, coord):
+def treeVisible(coord):
 	x, y = coord
-	# if x == 3:
-	# 	if y == 3:
-	# 		print("h")
-	for sub_x in range(x): # left side
-		if grid[y][sub_x] >= height:
+	height = data[y][x]
+	for sub_x in range(x):  # left side
+		if data[y][sub_x] >= height:
 			break
-	else:
+	else:  # didn't break: Tree visible from outside
 		return True
-	for sub_x in range(x+1, len(grid[y])):  # right side
-		if grid[y][sub_x] >= height:
+	for sub_x in range(x+1, len(data[y])):  # right side
+		if data[y][sub_x] >= height:
 			break
-	else:
+	else:  # didn't break: Tree visible from outside
 		return True
 	for sub_y in range(y):  # north direction
-		if grid[sub_y][x] >= height:
+		if data[sub_y][x] >= height:
 			break
-	else:
+	else:  # didn't break: Tree visible from outside
 		return True
-	for sub_y in range(y+1, len(grid)):  # south direction
-		if grid[sub_y][x] >= height:
+	for sub_y in range(y+1, len(data)):  # south direction
+		if data[sub_y][x] >= height:
 			break
-	else:
+	else:  # didn't break: Tree visible from outside
 		return True
-	return False
+	return False  # got here: tree is not visible
 
 
-def countVisTreesOut(grid):
+def countVisTreesOut():
 	visible_trees_out = set()
-	y_edge = len(grid)-1
-	x_edge = len(grid[0])-1
+	y_edge = len(data)-1
+	x_edge = len(data[0])-1
 	count = 1  # outside edge
-	for y, row in enumerate(grid):
-		for x, each in enumerate(grid[y]):
+	for y, row in enumerate(data):
+		for x, each in enumerate(data[y]):
 			if y == 0:  # all top trees are visible
 				visible_trees_out.add((x, y))
 				continue
@@ -56,27 +48,50 @@ def countVisTreesOut(grid):
 			elif x == x_edge:  # all right edge trees are visible
 				visible_trees_out.add((x, y))
 				continue
-			tree_height = grid[y][x]
-			if treeVisible(grid, tree_height, (x, y)):
+			if treeVisible((x, y)):
 				visible_trees_out.add((x, y))
 	return len(visible_trees_out)
 
 
-# for x, each in enumerate(grid[y]):
-# 	match y:
-# 		case 0:  # all top trees are visible
-# 			visible_trees.add((x, y))
-# 			continue
-# 		case yEdge:  # all bottom trees are visible
-# 			visible_trees.add((x, y))
-# 			continue
-# 	match x:
-# 		case 0:  # all left edge trees are visible
-# 			visible_trees.add((x, y))
-# 			continue
-# 		case x_edge:  # all right edge trees are visible
-# 			visible_trees.add((x, y))
+def visibleFromTree(coord):
+	x, y = coord
+	height = data[y][x]
+	countN, countS, countE, countW = 0, 0, 0, 0
+	for sub_x in range(x)[::-1]:  # left side
+		if data[y][sub_x] >= height:
+			countW += 1
+			break
+		else:
+			countW += 1
+	for sub_x in range(x+1, len(data[y])):  # right side
+		if data[y][sub_x] >= height:
+			countE +=1
+			break
+		else:
+			countE += 1
+	for sub_y in range(y)[::-1]:  # north direction
+		if data[sub_y][x] >= height:
+			countN += 1
+			break
+		else:
+			countN += 1
+	for sub_y in range(y+1, len(data)):  # south direction
+		if data[sub_y][x] >= height:
+			countS += 1
+			break
+		else:
+			countS += 1
+	return countN * countW * countE * countS
+
+
+def part2():
+	tree_scores = []
+	for y in range(len(data)):
+		for x in range(len(data[y])):
+			tree_scores.append(visibleFromTree((x, y)))
+	return max(tree_scores)
+
+
 if __name__ == "__main__":
-	# fun(matrix)
-	print("part 1: ", countVisTreesOut(matrix))
-	# print("part 2: ")
+	print("part 1: ", countVisTreesOut())
+	print("part 2: ", part2())
