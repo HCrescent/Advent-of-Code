689725335871203cb5c50e8c89f20095f90c1b95
HCrescent <HCrescentMoon@gmail.com>
2022-12-14 19:55:29 -0600

    2022 day 13 part 2 complete

--- 2022/Python/day13.py
+++ 2022/Python/day13.py
@@ -11,52 +11,61 @@ def part1():
 	correct_list = []
 	for i in range(len(data)-1)[::2]:
 		if processTwoLists(data[i], data[i+1]):
-			correct_list.append((i//2)+1)
+			correct_list.append((i//2)+1)  # index by pair
 	return sum(correct_list)
 
 
+# noinspection PyTypeChecker
 def part2():
+	# inefficient sorting
 	while True:
 		for i in range(len(data)-1):
-			if processTwoLists(data[i], data[i+1]):
-				continue
-			else:  # swap the two
-				break
+			if data[i] != data[i+1]:  # don't bother with equivalent packets
+				if processTwoLists(data[i], data[i+1]):
+					continue
+				else:  # swap the two
+					break
 		else:  # list was fully sorted
-			return
-		data[i], data[i+1] = data[i+1], data[i]
+			divider1_i = data.index(divider1)+1
+			divider2_i = data.index(divider2)+1
+			return divider1_i * divider2_i
+		data[i], data[i+1] = data[i+1], data[i]  # swap
 
 
-def processTwoLists(left, right):
-	for i, each in enumerate(right):  # enumerate right list to match with left
+def processTwoLists(left_list, right_list):
+	for i, each in enumerate(right_list):  # enumerate right list to match with left
 		try:
-			if left[i] == each:  # no determination yet keep going
-				continue  # next iteration
-			return callme(left[i], each)  # test left element and right element
+			if left_list[i] == each:  # no determination yet keep going
+				continue
+			test = testLR(left_list[i], each)  # test left element and right element if they are different
+			if test is None:  # catches int, list being equivalent AFTER conversion
+				continue
+			return test
 		except IndexError:  # if left[i] produces index error
 			return True  # left list was shorter we reached the conclusion that we have correct order
+	else:  # we finished through the list, if they are equivalent we return None
+		if len(right_list) < len(left_list):
+			return False  # list out of order
 
 
-def callme(left, right):
+def testLR(left, right):
 	match (left, right):  # match the type of comparison going on this time
 		case (list(), list()):  # list vs list
 			return processTwoLists(left, right)
-		case (int(), list()) | (list(), int()):  # integer vs list  the problem case
+		case (int(), list()) | (list(), int()):  # integer vs list
 			if isinstance(left, int):  # if left is the int
 				left = [left]
 			if isinstance(right, int):  # if right is the int
 				right = [right]
-			return callme(left, right)  # two lists called
+			return processTwoLists(left, right)  # two lists called
 		case (int(), int()):
 			return left < right  # if true we are all done go back up and out
 
 
 if __name__ == "__main__":
-	print(part1())
-	data.append([[2]])
-	data.append([[6]])
-	part2()
-	for each in data:
-		print(each)
-	# print("part 1: ")
-	# print("part 2: ")
+	print("part 1: ", part1())
+	divider1 = [[2]]
+	divider2 = [[6]]
+	data.append(divider1)
+	data.append(divider2)
+	print("part 2: ", part2())
