124a66f91298ec2256c2ad33d8b3ecb4a99fd149
HCrescent <HCrescentMoon@gmail.com>
2022-12-20 15:24:50 -0600

    2022 day 20 part 1 done

--- 2022/Python/day20.py
+++ 2022/Python/day20.py
@@ -1,46 +1,30 @@
 """Day 20 Advent_of_Code 2022"""
 with open("input/day20.txt", 'r') as infile:
 	data = [int(line.rstrip()) for line in infile]
-enumeration = [i for i in range(len(data))]
-print(data)
-print(enumeration)
 
 
-def fun():
+def mix():
+	enumeration = [i for i in range(len(data))]
 	msg_len = len(enumeration)
 	for procession in range(msg_len):
 		target_adr = enumeration.index(procession)
 		tmp_dat = data.pop(target_adr)
 		tmp_enum = enumeration.pop(target_adr)
-		if tmp_dat < 0:
-			new_adr = ((target_adr + tmp_dat) % msg_len) - 1
+		if tmp_dat != 0:
+			new_adr = (target_adr + tmp_dat) % (msg_len-1)
 		else:
-			new_adr = (target_adr + tmp_dat) % msg_len
-			if tmp_dat + target_adr > msg_len:
-				new_adr += 1 * ((target_adr + tmp_dat)//msg_len)  # plus one for each wrap around
-		if new_adr == -1:  # insert -1 doesnt work the same as slicing
-			data.append(tmp_dat)
-			enumeration.append(tmp_enum)
-			print(procession)
-			print(data)
-			print(enumeration)
-			print()
-			continue
-		if new_adr < 0:
-			new_adr += 1
+			new_adr = target_adr
 		data.insert(new_adr, tmp_dat)
 		enumeration.insert(new_adr, tmp_enum)
-		print(procession)
-		print(data)
-		print(enumeration)
-		print()
 	pass
 
 
 if __name__ == "__main__":
-	test = [1, 2, 3]
-	test.insert(-2, 4)
-	print(test)
-	fun()
-	# print("part 1: ")
+	mix()
+	start_point = data.index(0)
+	msg = len(data)
+	a = data[(start_point + 1000) % msg]
+	b = data[(start_point + 2000) % msg]
+	c = data[(start_point + 3000) % msg]
+	print("part 1: ", a+b+c)
 	# print("part 2: ")
