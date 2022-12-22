40581f14515121c478bc462bc41c563c2e1da781
HCrescent <HCrescentMoon@gmail.com>
2022-12-22 16:39:27 -0600

    2022 day 22 part 1 correct checkpoint

--- 2022/Python/day22.py
+++ 2022/Python/day22.py
@@ -6,7 +6,7 @@ paces = [int(each) for each in directions.replace('R', 'L').split('L')]
 left_right = [char for char in directions if char.isalpha()]
 forest_map = data[0].split('\n')
 forest_map = [[char for char in row] for row in forest_map]
-
+real_space = {'.', '#'}
 turn = {
 	'L': {'^': '<', '<': 'v', 'v': '>', '>': '^'},
 	'R': {'^': '>', '<': '^', 'v': '<', '>': 'v'}
@@ -19,74 +19,69 @@ for y in range(len(forest_map)):
 		if forest_map[y][x] == '#':
 			walls.add((x, y))
 score = {'>': 0, 'v': 1, '<': 2, '^': 3}
+path = set(start_coord)  # just in case we want to make a nice visual representation later
 
 
 def walk():
-	cardinal = '>'
-	current = list(start_coord)
-	# forest_map[current[1]][current[0]] = cardinal
-	# print(current)
-	# for c, v in enumerate(paces):
-	# 	print(c, v)
-	# print(len(paces))
-	# print(len(left_right))
+	def subFun():
+		return
+	cardinal = '>'  # cardinal will be the orientation we are facing
+	current = list(start_coord)  # current will be our location before the walk step
 	for i, steps in enumerate(paces):  # for each instruction steps + turn
 		for _ in range(steps):  # for each in the integer number of steps
+			# next find the proper coordinate we want to move to
+			# temp coord is the current coord plus the proper cardinal adjustment
+			tmp_x, tmp_y = [sum(coord) for coord in zip(current, move[cardinal])]
+			# exception handles wrap cases
 			try:
-				if forest_map[current[1]][current[0]] != '.':
-					print(current)
+				# treat whitespace tiles as index errors for less redundant case coverage
+				if forest_map[tmp_y][tmp_x] == ' ' or tmp_y < 0 or tmp_x < 0:
+					raise IndexError
 			except IndexError:
-				print(current)
-				for each in forest_map[current[1]-5:current[1]+6]:
-					print("".join(each))
-				raise exit(1)
-			tmp_coord = [current[0] + move[cardinal][0], current[1] + move[cardinal][1]]
-			# wrapping logic
-			try:
-				if forest_map[tmp_coord[1]][tmp_coord[0]] == ' ':
-					tmp_x = tmp_coord[0]
-					tmp_y = tmp_coord[1]
-					if cardinal == 'v':
-						for row in range(len(forest_map)):
-							if forest_map[row][tmp_x] != ' ':
-								tmp_coord = [tmp_x, row]
-								break
-					if cardinal == '^':
+				match cardinal:
+					case '<':  # exiting left coming in right
+						# we want our x to be the rightmost edge, minus 1 for zero indexing
+						tmp_x = len(forest_map[tmp_y])-1
+					case '>':  # exiting right coming in left
+						# left most element that is not a whitespace could be either '.' or '#'
+						tmp_x = min(forest_map[tmp_y].index('.'), forest_map[tmp_y].index('#'))
+					case '^':  # exiting top and coming in bottom
+						# for each row from bottom to top, find the first y level that's a '.' or '#'
 						for row in range(len(forest_map))[::-1]:
-							if forest_map[row][tmp_x] != ' ':
-								tmp_coord = [tmp_x, row]
-								break
-					if cardinal == '<':
-						tmp_coord[0] = len(forest_map[tmp_coord[1]]) - 1
-			except IndexError:
-				if cardinal == '>':
-					tmp_coord[0] = forest_map[tmp_coord[1]].index('.')
-				if cardinal == '<':
-					tmp_coord[0] = len(forest_map[tmp_coord[1]])-1
-				if cardinal == '^':
-					for row_i in range(len(forest_map))[::-1]:
-						try:
-							if forest_map[row_i][tmp_coord[0]] != " ":
-								tmp_coord[1] = row_i
-								break
-						except IndexError:
-							tmp_coord[1] = row_i-1
-				if cardinal == 'v':
-					for row_i in range(len(forest_map)):
-						if forest_map[row_i][tmp_coord[0]] != " ":
-							tmp_coord[1] = row_i
-							break
+							try:  # if index error or whitespace we go on to next row
+								if forest_map[row][tmp_x] in real_space:
+									tmp_y = row
+									break  # don't overwrite first find
+							except IndexError:
+								continue
+					case 'v':  # exiting bottom and coming in top
+						for row in range(len(forest_map)):
+							try:  # if index error or whitespace we go on to next row
+								if forest_map[row][tmp_x] in real_space:
+									tmp_y = row
+									break  # don't overwrite fist find
+							except IndexError:
+								continue
+			# temporary coordinate to check if is a wall
+			tmp_coord = [tmp_x, tmp_y]
 			if tuple(tmp_coord) in walls:
 				break
 			current = tmp_coord
-			# for each in forest_map:
+			# for row, each in enumerate(forest_map):
+			# 	if row == current[1]:
+			# 		tmp_char = forest_map[row][current[0]]
+			# 		each[current[0]] = '@'
 			# 	print("".join(each))
+			# 	if row == current[1]:
+			# 		each[current[0]] = tmp_char
 			# print()
 		cardinal = turn[left_right[i]][cardinal]
 		if i == len(left_right)-1:
 			break
 	current[0] += 1
 	current[1] += 1
+
+	# given we found the correct end space and orientation, calculate the answer
 	answer = 1000 * current[1]
 	answer2 = 4 * current[0]
 	answer3 = score[cardinal]
@@ -95,6 +90,5 @@ def walk():
 
 
 if __name__ == "__main__":
-	print(walk())
-	# print("part 1: ")
+	print("part 1: ", walk())
 	# print("part 2: ")
