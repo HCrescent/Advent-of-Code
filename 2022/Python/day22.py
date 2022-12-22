aa6828b222ee2dfe7f25e7db1cb6578f44c9893c
HCrescent <HCrescentMoon@gmail.com>
2022-12-22 14:06:11 -0600

    2022 day 22 checkpoint

--- 2022/Python/day22.py
+++ 2022/Python/day22.py
@@ -18,38 +18,83 @@ for y in range(len(forest_map)):
 	for x in range(len(forest_map[y])):
 		if forest_map[y][x] == '#':
 			walls.add((x, y))
+score = {'>': 0, 'v': 1, '<': 2, '^': 3}
 
 
 def walk():
 	cardinal = '>'
 	current = list(start_coord)
-	forest_map[current[1]][current[0]] = cardinal
-	print(current)
+	# forest_map[current[1]][current[0]] = cardinal
+	# print(current)
+	# for c, v in enumerate(paces):
+	# 	print(c, v)
+	# print(len(paces))
+	# print(len(left_right))
 	for i, steps in enumerate(paces):  # for each instruction steps + turn
 		for _ in range(steps):  # for each in the integer number of steps
-			forest_map[current[1]][current[0]] = cardinal
+			try:
+				if forest_map[current[1]][current[0]] != '.':
+					print(current)
+			except IndexError:
+				print(current)
+				for each in forest_map[current[1]-5:current[1]+6]:
+					print("".join(each))
+				raise exit(1)
 			tmp_coord = [current[0] + move[cardinal][0], current[1] + move[cardinal][1]]
+			# wrapping logic
+			try:
+				if forest_map[tmp_coord[1]][tmp_coord[0]] == ' ':
+					tmp_x = tmp_coord[0]
+					tmp_y = tmp_coord[1]
+					if cardinal == 'v':
+						for row in range(len(forest_map)):
+							if forest_map[row][tmp_x] != ' ':
+								tmp_coord = [tmp_x, row]
+								break
+					if cardinal == '^':
+						for row in range(len(forest_map))[::-1]:
+							if forest_map[row][tmp_x] != ' ':
+								tmp_coord = [tmp_x, row]
+								break
+					if cardinal == '<':
+						tmp_coord[0] = len(forest_map[tmp_coord[1]]) - 1
+			except IndexError:
+				if cardinal == '>':
+					tmp_coord[0] = forest_map[tmp_coord[1]].index('.')
+				if cardinal == '<':
+					tmp_coord[0] = len(forest_map[tmp_coord[1]])-1
+				if cardinal == '^':
+					for row_i in range(len(forest_map))[::-1]:
+						try:
+							if forest_map[row_i][tmp_coord[0]] != " ":
+								tmp_coord[1] = row_i
+								break
+						except IndexError:
+							tmp_coord[1] = row_i-1
+				if cardinal == 'v':
+					for row_i in range(len(forest_map)):
+						if forest_map[row_i][tmp_coord[0]] != " ":
+							tmp_coord[1] = row_i
+							break
 			if tuple(tmp_coord) in walls:
 				break
-			else:
-				# wrapping logic
-				try:
-					if forest_map[tmp_coord[1]][tmp_coord[0]] == '':
-						pass
-				except IndexError:
-					tmp_coord[0] = forest_map[tmp_coord[1]].index('.')
-				current = tmp_coord
-				for each in forest_map:
-					print("".join(each))
-				print()
+			current = tmp_coord
+			# for each in forest_map:
+			# 	print("".join(each))
+			# print()
 		cardinal = turn[left_right[i]][cardinal]
-
-	return
+		if i == len(left_right)-1:
+			break
+	current[0] += 1
+	current[1] += 1
+	answer = 1000 * current[1]
+	answer2 = 4 * current[0]
+	answer3 = score[cardinal]
+	summation = answer+answer2+answer3
+	return summation
 
 
 if __name__ == "__main__":
-	for each in data:
-		print(each)
-	walk()
+	print(walk())
 	# print("part 1: ")
 	# print("part 2: ")
