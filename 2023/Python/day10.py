25992357a5c960f35bf944f4b05b06d2d97525e4
HCrescent <HCrescentMoon@gmail.com>
2023-12-28 17:54:41 -0600

    Aoc day 10 rollback commit for rewrite

--- 2023/Python/day10.py
+++ 2023/Python/day10.py
@@ -13,18 +13,24 @@ pipe_dict = {'|':('N', 'S'), '-':('E', 'W'), 'F':('E', 'S'), '7':('S', 'W'), 'J'
 class Pipe:
 	def __init__(self, coordinate, steps):
 		self.coord = tuple(coordinate)
-		self.pipeType = coordinate[0][1]
+		self.pipeType = data[coordinate[0]][coordinate[1]]
 		self.connectors = pipe_dict[self.pipeType]
-		self.steps = steps+1
+		self.steps = steps
 
 	def __eq__(self, other):
 		return self.steps == other.steps
+
+	def __ne__(self, other):
+		return self.coord != other.coord
 
 	def __lt__(self, other):
 		return self.steps < other.steps
 
 	def __hash__(self):
 		return hash(self.coord)
+
+	def __repr__(self):
+		return f"{self.pipeType}: {self.coord} steps: {self.steps}/"
 
 
 def printPipes(coord):
@@ -35,6 +41,7 @@ def printPipes(coord):
 	temp_p[coord[0]] = temp_p[coord[0]][:coord[1]] + replacement + temp_p[coord[0]][coord[1]+1:]
 	for _ in temp_p:
 		print(_)
+	print()
 	return
 
 
@@ -44,14 +51,12 @@ def determineStart():
 		for i2, character in enumerate(line):
 			if character == 'S':
 				start_location = (i1, i2)
-				printPipes(start_location)
 				break
 		if start_location is not None:
 			break
 	pipe_entrances = []
 	surroundings = [[sum(pair) for pair in zip(start_location, move_dict[_])] for _ in ['N','E','S','W']]
 	# if north pipe has a south entrance, We know Starter pipe has a North entrance
-
 	try:
 		if pipe_dict[data[surroundings[0][0]][surroundings[0][1]]].count('S') == 1:
 			pipe_entrances.append('N')
@@ -75,17 +80,44 @@ def determineStart():
 			pipe_entrances.append('W')
 	except IndexError:
 		pass
-	print(pipe_entrances)
-	pipe = {k for k, v in pipe_dict.items() if v == tuple(pipe_entrances)}
-	print(pipe)
+	pipe = [k for k, v in pipe_dict.items() if v == tuple(pipe_entrances)]
+	data[start_location[0]] = data[start_location[0]].replace('S', pipe[0])
+	printPipes(start_location)
+	return start_location
 
 
-def fun():
-	pass
+def traverse(start_node, flag=True):
+	positive = {start_node}
+	steps = 1
+	if flag:
+		current_node = Pipe([sum(_) for _ in zip(start_node.coord, move_dict[pipe_dict[start_node.pipeType][0]])], 1)
+	else:
+		current_node = Pipe([sum(_) for _ in zip(start_node.coord, move_dict[pipe_dict[start_node.pipeType][1]])], 1)
+	positive.add(current_node)
+	next_node = current_node
+	while True:
+		print(steps)
+		for n in range(2):
+			next_node = Pipe([sum(_) for _ in zip(current_node.coord, move_dict[current_node.connectors[n]])], steps+1)
+			if next_node not in positive:
+				current_node = next_node
+				positive.add(current_node)
+				steps+=1
+				break
+		else:
+			return list(positive)
+
+
+def part1():
+	start_node = Pipe(determineStart(), 0)
+	resultp = traverse(start_node)
+	resultn = traverse(start_node, False)
+	print(resultp)
+	print(resultn)
+	return
 
 
 if __name__ == "__main__":
-	determineStart()
 	print('\x1b[6;30;42m' + 'Success!' + '\x1b[0m')
-	# print("part 1: ")
+	print("part 1: ", part1())
 	# print("part 2: ")
