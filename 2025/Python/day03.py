56db75f07ee9a8d32c176cce833a7bc6ed54c38f
HCrescent <HCrescentMoon@gmail.com>
2025-12-03 03:53:39 -0600

    Aoc 2025 day 03 done

--- 2025/Python/day03.py
+++ 2025/Python/day03.py
@@ -2,34 +2,30 @@
 with open("input/day03.txt", 'r') as infile:
 	data = [line.rstrip() for line in infile]
 data = [[int(digits) for digits in each] for each in data]
-
-def part1():
-	total = 0
-	for bank in data:
-		tens = max(bank[:-1])
-		position = bank.index(tens)+1
-		ones = max(bank[position:])
-		total += tens * 10 + ones
-	return total
-
+#
+# def part1():
+# 	total = 0
+# 	for bank in data:
+# 		tens = max(bank[:-1])
+# 		position = bank.index(tens)+1
+# 		ones = max(bank[position:])
+# 		total += tens * 10 + ones
+# 	return total
+#
 
 def part2(length):
-	N = length - 1
 	total = 0
 	for bank in data:
-		lead = max(bank[:-N])
-		position = bank.index(lead)
-		bank = bank[position:]
-		while len(bank) > length:
-			bobby_tables = min(bank)
-			bank.remove(bobby_tables)
-		constructed_number = 0
-		for each in bank:
-			constructed_number = constructed_number * 10 + each
-		total += constructed_number
+		constructed_num = 0
+		for n in range(length)[::-1]:
+			lead = max(bank[:len(bank)-n])
+			position = bank.index(lead)+1
+			bank = bank[position:]
+			constructed_num = constructed_num * 10 + lead
+		total += constructed_num
 	return total
 
 
 if __name__ == "__main__":
-	print("part 1: ", part1())
+	print("part 1: ", part2(2))
 	print("part 2: ", part2(12))
